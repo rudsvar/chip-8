fb3d6cf1d9635c0f6a5c0ec188dde35ed4f92322
Rudi B. Svartveit <rudsvar@gmail.com>
2020-07-22 23:26:32 +0200

    Finish parsing of opcodes up until BitshiftRight

--- src/instruction.rs
+++ src/instruction.rs
@@ -30,7 +30,7 @@ pub enum Instruction {
     BitwiseXor(Reg, Reg),
     IncRegByReg(Reg, Reg),
     DecRegByReg(Reg, Reg),
-    BitshiftRight(Reg, Const)
+    BitshiftRight(Reg)
 }
 
 impl Instruction {
@@ -57,6 +57,13 @@ impl Instruction {
             (4, x, _, _) => Instruction::IfRegNeqConst(Reg(x), Const(opcode.last_8_bits())),
             (5, x, y, 0) => Instruction::IfRegEqReg(Reg(x), Reg(y)),
             (6, x, _, _) => Instruction::SetRegToConst(Reg(x), Const(opcode.last_8_bits())),
+            (7, x, _, _) => Instruction::IncRegByConst(Reg(x), Const(opcode.last_8_bits())),
+            (8, x, y, 1) => Instruction::BitwiseOr(Reg(x), Reg(y)),
+            (8, x, y, 2) => Instruction::BitwiseAnd(Reg(x), Reg(y)),
+            (8, x, y, 3) => Instruction::BitwiseXor(Reg(x), Reg(y)),
+            (8, x, y, 4) => Instruction::IncRegByReg(Reg(x), Reg(y)),
+            (8, x, y, 5) => Instruction::DecRegByReg(Reg(x), Reg(y)),
+            (8, x, _, 6) => Instruction::BitshiftRight(Reg(x)),
             _ => panic!("Unknown opcode!") // TODO: Use Option?
         }
     }
@@ -65,7 +72,27 @@ impl Instruction {
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
+
+    #[test]
+    fn opcodes_are_parsed_correctly() {
+        assert_eq!(Instruction::ClearScreen, Instruction::from_u16(0x00E0));
+        assert_eq!(Instruction::Return, Instruction::from_u16(0x00EE));
+        assert_eq!(Instruction::Goto(Addr(0x25)), Instruction::from_u16(0x1025));
+        assert_eq!(Instruction::Call(Addr(0x37)), Instruction::from_u16(0x2037));
+        assert_eq!(Instruction::IfRegEqConst(Reg(0xA), Const(8)), Instruction::from_u16(0x3A08));
+        assert_eq!(Instruction::IfRegNeqConst(Reg(0xA), Const(8)), Instruction::from_u16(0x4A08));
+        assert_eq!(Instruction::IfRegNeqConst(Reg(0xA), Const(8)), Instruction::from_u16(0x4A08));
+        assert_eq!(Instruction::SetRegToConst(Reg(0xB), Const(0x23)), Instruction::from_u16(0x6B23));
+        assert_eq!(Instruction::IncRegByConst(Reg(0xC), Const(0xA1)), Instruction::from_u16(0x7CA1));
+        assert_eq!(Instruction::BitwiseOr(Reg(0xD), Reg(0xE)), Instruction::from_u16(0x8DE1));
+        assert_eq!(Instruction::BitwiseAnd(Reg(0xD), Reg(0xE)), Instruction::from_u16(0x8DE2));
+        assert_eq!(Instruction::BitwiseXor(Reg(0xD), Reg(0xE)), Instruction::from_u16(0x8DE3));
+        assert_eq!(Instruction::IncRegByReg(Reg(0xA), Reg(0xB)), Instruction::from_u16(0x8AB4));
+        assert_eq!(Instruction::DecRegByReg(Reg(0xA), Reg(0xB)), Instruction::from_u16(0x8AB5));
+        assert_eq!(Instruction::BitshiftRight(Reg(0xA)), Instruction::from_u16(0x8AB6));
+    }
 
     #[test]
     fn from_two_u8_equals_from_u16() {
@@ -82,13 +109,5 @@ mod tests {
         assert_eq!((0x00, 0x00), Instruction::split_u16(0x0000));
         assert_eq!((0xF0, 0xF0), Instruction::split_u16(0xF0F0));
 
-    }
-
-    #[test]
-    fn opcodes_are_parsed_correctly() {
-        assert_eq!(Instruction::ClearScreen, Instruction::from_u16(0x00E0));
-        assert_eq!(Instruction::Return, Instruction::from_u16(0x00EE));
-        assert_eq!(Instruction::Goto(Addr(0x25)), Instruction::from_u16(0x1025));
-        assert_eq!(Instruction::Call(Addr(0x37)), Instruction::from_u16(0x2037));
     }
 }
