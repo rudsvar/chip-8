00a93421ec29550d354a6c513138ec67db67cb92
Rudi B. Svartveit <rudsvar@gmail.com>
2020-07-23 13:33:14 +0200

    Add more emulator fields

--- src/emulator.rs
+++ src/emulator.rs
@@ -5,7 +5,12 @@ use crate::instruction::Instruction;
 pub struct Emulator {
     memory: [u8; 4096],
     registers: [u8; 16],
-    pc: usize,
+    delay_timer: u8,
+    sound_timer: u8,
+    I: u16,
+    PC: u16,
+    SP: u8,
+    stack: [u16; 256],
     screen: [[bool; 64]; 32]
 }
 
@@ -16,7 +21,12 @@ impl Emulator {
         let mut emulator = Emulator {
             memory: [0; 4096],
             registers: [0; 16],
-            pc: 512,
+            delay_timer: 0,
+            sound_timer: 0,
+            I: 0,
+            PC: 0x200,
+            SP: 0,
+            stack: [0; 256],
             screen: [[false; 64]; 32]
         };
         for i in 0..std::cmp::min(memory.len(), emulator.memory.len()) {
@@ -28,10 +38,10 @@ impl Emulator {
     /// Perform a single step by executing one instruction
     pub fn step(&mut self) -> bool {
         // Each opcode is two bytes
-        let left = self.memory[self.pc];
-        let right = self.memory[self.pc + 1];
+        let left = self.memory[self.PC as usize];
+        let right = self.memory[self.PC as usize + 1];
         let instruction = Instruction::from_two_u8(left, right);
-        self.pc += 2;
+        self.PC += 2;
 
         match instruction {
             _ => {
