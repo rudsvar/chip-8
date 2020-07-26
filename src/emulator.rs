6cad73a0c19c2ddb8f3acead3bef1db06b2cd5ab
Rudi B. Svartveit <rudsvar@gmail.com>
2020-07-26 17:17:58 +0200

    Refactor to make keypresses more responsive

--- src/emulator.rs
+++ src/emulator.rs
@@ -5,13 +5,41 @@ use crate::instruction::*;
 const MEM_SIZE: usize = 4096;
 const NUM_REGISTERS: usize = 16;
 const STACK_SIZE: usize = 256;
-const SCREEN_WIDTH: usize = 128;
-const SCREEN_HEIGHT: usize = 128;
-type Screen = [[u8; SCREEN_WIDTH]; SCREEN_HEIGHT];
-const EMPTY_SCREEN: Screen = [[0; SCREEN_WIDTH]; SCREEN_HEIGHT];
+// const SCREEN_WIDTH: usize = 128;
+// const SCREEN_HEIGHT: usize = 128;
+// type Screen = [[u8; SCREEN_WIDTH]; SCREEN_HEIGHT];
+// const EMPTY_SCREEN: Screen = [[0; SCREEN_WIDTH]; SCREEN_HEIGHT];
 const PC_START: u16 = 0x200;
 
-pub struct Emulator {
+pub trait Input {
+    fn get_key(&self) -> Option<u8>;
+    fn get_key_blocking(&self) -> u8;
+}
+
+pub struct DummyInput;
+
+impl Input for DummyInput {
+    fn get_key(&self) -> Option<u8> { None }
+    fn get_key_blocking(&self) -> u8 { unimplemented!("Can't get blocking") }
+}
+
+pub trait Output {
+    fn set(&mut self, x: usize, y: usize, state: u8);
+    fn get(&self, x: usize, y: usize) -> u8;
+    fn clear(&mut self);
+    fn refresh(&mut self);
+}
+
+pub struct DummyOutput;
+
+impl Output for DummyOutput {
+    fn set(&mut self, _: usize, _: usize, state: u8) {}
+    fn get(&self, _: usize, _: usize) -> u8 { 0 }
+    fn clear(&mut self) {}
+    fn refresh(&mut self) {}
+}
+
+pub struct Emulator<I: Input, O: Output> {
     // Standard fields
     memory: [u8; MEM_SIZE],
     registers: [u8; NUM_REGISTERS],
@@ -21,15 +49,15 @@ pub struct Emulator {
     program_counter: u16,
     stack_pointer: u8,
     stack: [u16; STACK_SIZE],
-    screen: Screen,
-    // Custom fields
-    pixels_to_redraw: Vec<(usize, usize)>
+
+    input: I,
+    output: O
 }
 
-impl Emulator {
+impl<I: Input, O: Output> Emulator<I, O> {
 
-    /// Create a new emulator
-    pub fn new() -> Emulator {
+    /// Create a new emulator with input and output
+    pub fn with_io(input: I, output: O) -> Emulator<I, O> {
         Emulator {
             memory: [0; MEM_SIZE],
             registers: [0; NUM_REGISTERS],
@@ -39,31 +67,15 @@ impl Emulator {
             program_counter: PC_START,
             stack_pointer: 0,
             stack: [0; STACK_SIZE],
-            screen: EMPTY_SCREEN,
-            pixels_to_redraw: Vec::new()
+            
+            input,
+            output
         }
     }
 
-    /// Get access to the entire game screen.
-    pub fn get_screen(&self) -> &Screen {
-        &self.screen
-    }
-
-    /// Get a list of all positions that have been updated during the previous step.
-    pub fn get_screen_update_locations(&self) -> &Vec<(usize, usize)> {
-        &self.pixels_to_redraw
-    }
-
-    /// Tell the screen to update all drawn pixels.
-    pub fn flag_all_pixels_for_redrawing(&mut self) {
-        self.pixels_to_redraw = Vec::new();
-        for y in 0..SCREEN_HEIGHT {
-            for x in 0..SCREEN_WIDTH {
-                if self.screen[y][x] == 1 {
-                    self.pixels_to_redraw.push((x,y));
-                }
-            }
-        }
+    /// Create a new emulator with dummy input and output
+    pub fn new() -> Emulator<DummyInput, DummyOutput> {
+        Emulator::with_io(DummyInput, DummyOutput)
     }
 
     /// Copy a program into memory at 0x200.
@@ -75,7 +87,7 @@ impl Emulator {
 
     /// Perform a single step, which will update timers,
     /// then load an instruction and execute it.
-    pub fn step(&mut self, keys_pressed: &[bool; 0xF]) -> bool {
+    pub fn step(&mut self) {
 
         // Update timers
         if self.delay_timer > 0 {
@@ -93,21 +105,18 @@ impl Emulator {
 
         log::trace!("{:?}", instruction);
 
-        self.pixels_to_redraw = Vec::new();
-
         self.program_counter += 2; // TODO: Make this more error resistant?
 
-        self.execute_single(instruction, keys_pressed)
+        self.execute_single(instruction);
     }
 
     /// Execute a single instruction
-    fn execute_single(&mut self, instruction: Instruction, keys_pressed: &[bool; 0xF]) -> bool {
+    fn execute_single(&mut self, instruction: Instruction) {
         match instruction {
 
             // Clear the screen
             Instruction::ClearScreen => {
-                self.flag_all_pixels_for_redrawing();
-                self.screen = EMPTY_SCREEN;
+                self.output.clear();
             },
 
             // Return to the previous call site via the stack.
@@ -232,17 +241,14 @@ impl Emulator {
                     let row: u8 = sprite_data[y];
                     for x in 0 .. 8 {
                         let new_pixel = (row >> (8 - x)) & 1; // Get bit number `bit_idx`
-                        let old_pixel = &mut self.screen[y_coord + y][x_coord + x];
-                        let xored_pixel = *old_pixel ^ new_pixel; // XOR old pixel with new pixel
-                        // Set pixel is unset
-                        if *old_pixel == 1 && xored_pixel == 0 {
+                        let old_pixel = self.output.get(x_coord + x, y_coord + y);
+                        let xored_pixel = old_pixel ^ new_pixel; // XOR old pixel with new pixel
+                        self.output.set(x_coord + x, y_coord + y, xored_pixel); // Save xor'ed pixel
+
+                        // Set pixel was unset, so we set the collision flag
+                        if old_pixel == 1 && xored_pixel == 0 {
                             any_collisions = 1;
                         }
-                        // Store updated locations
-                        if *old_pixel != xored_pixel {
-                            self.pixels_to_redraw.push((x_coord + x, y_coord + y));
-                        }
-                        *old_pixel = xored_pixel; // Save xor'ed pixel
                     }
                 }
 
@@ -252,14 +258,14 @@ impl Emulator {
 
             // TODO: Skip if the key in Vx is pressed
             Instruction::IfKeyEqVx(Reg(x)) => {
-                if keys_pressed[self.registers[x as usize] as usize] {
+                if self.input.get_key() == Some(self.registers[x as usize]) {
                     self.program_counter += 2;
                 }
             }
 
             // TODO: Skip if the key in Vx isn't pressed
             Instruction::IfKeyNeqVx(Reg(x)) => {
-                if !keys_pressed[self.registers[x as usize] as usize] {
+                if self.input.get_key() != Some(self.registers[x as usize]) {
                     self.program_counter += 2;
                 }
             }
@@ -269,8 +275,8 @@ impl Emulator {
             }
 
             // Get a key press (blocking)
-            Instruction::SetRegToGetKey(Reg(_)) => {
-                log::error!("Unimplemented: SetRegToGetKey");
+            Instruction::SetRegToGetKey(Reg(x)) => {
+                self.registers[x as usize] = self.input.get_key_blocking();
             }
 
             Instruction::SetDelayTimerToReg(Reg(x)) => {
@@ -288,6 +294,7 @@ impl Emulator {
             // TODO: Add font sprites?
             Instruction::SetIToSpriteAddrVx(Reg(_)) => {
                 log::warn!("Unimplemented: SetIToSpriteAddrVx");
+                self.i = 0x0; // Set to empty area
             }
 
             Instruction::SetIToBcdOfReg(Reg(x)) => {
@@ -322,8 +329,6 @@ impl Emulator {
                 }
             }
         };
-
-        true
     }
 }
 
@@ -336,14 +341,14 @@ mod tests {
     fn clear_screen_clears_screen() {
         let mut emulator = Emulator::new();
         emulator.screen[0][0] = 1;
-        emulator.execute_single(Instruction::ClearScreen, [false; 0xF]);
+        emulator.execute_single(Instruction::ClearScreen);
         assert_eq!(emulator.screen[0][0], 0);
     }
 
     #[test]
     fn goto_goes_to() {
         let mut emulator = Emulator::new();
-        emulator.execute_single(Instruction::Goto(Addr(0x250)), [false; 0xF]);
+        emulator.execute_single(Instruction::Goto(Addr(0x250)));
         assert_eq!(emulator.program_counter, 0x250);
     }
 
@@ -363,9 +368,9 @@ mod tests {
         emulator.load(&program);
 
         // Run the program
-        emulator.step([false; 0xF]); // Call 0x206
+        emulator.step(); // Call 0x206
         assert_eq!(emulator.program_counter, 0x206);
-        emulator.step([false; 0xF]); // Return to 202
+        emulator.step(); // Return to 202
         assert_eq!(emulator.program_counter, 0x202);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::path::PathBuf;
 use structopt::StructOpt;
 use chip_8::emulator::Emulator;
-use std::io::{Write, stdout};
+use chip_8::emulator::{Input, Output};
+use std::io::{Write, stdout, Stdout};
+use std::thread;
+use std::sync::{Arc, Mutex, mpsc};
+use std::time;
 
 use termion::screen::AlternateScreen;
-use termion::event::{Key, Event};
+use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
@@ -28,6 +32,133 @@ struct Opt {
     input: PathBuf,
 }
 
+struct TermionInput {
+    key: Arc<Mutex<Option<(u8, time::SystemTime)>>>, // Latest pressed key
+    waiting: Arc<Mutex<bool>>, // Wether we are waiting for a keypress or not
+    receiver: mpsc::Receiver<(u8, time::SystemTime)>, // Channel for receiving blocking keypresses
+}
+
+impl TermionInput {
+    fn new(key: Arc<Mutex<Option<(u8, time::SystemTime)>>>, waiting: Arc<Mutex<bool>>, receiver: mpsc::Receiver<(u8, time::SystemTime)>) -> TermionInput {
+        TermionInput {
+            key,
+            waiting,
+            receiver,
+        }
+    }
+}
+
+impl Input for TermionInput {
+    fn get_key(&self) -> Option<u8> {
+        match *self.key.lock().unwrap() {
+            Some((key, timestamp)) => {
+                if timestamp.elapsed().unwrap() < time::Duration::from_millis(250) {
+                    log::info!("Key pressed is {}", key);
+                    return Some(key);
+                }
+            },
+            _ => {}
+        }
+
+        None
+    }
+    fn get_key_blocking(&self) -> u8 {
+        let mut waiting = self.waiting.lock().unwrap();
+        *waiting = true;
+        let mut result = 0;
+        for (key, timestamp) in self.receiver.recv() {
+            if timestamp.elapsed().unwrap() < time::Duration::from_millis(250) {
+                result = key;
+                break;
+            }
+        }
+        *waiting = false;
+        result
+    }
+}
+
+struct TermionOutput {
+    screen: AlternateScreen<termion::raw::RawTerminal<Stdout>>,
+    cells: [[u8; 128]; 128]
+}
+
+impl TermionOutput {
+    fn new() -> TermionOutput {
+        let mut screen = AlternateScreen::from(stdout().into_raw_mode().unwrap());
+        write!(screen, "{}", termion::cursor::Hide).unwrap();
+        TermionOutput {
+            screen,
+            cells: [[0; 128]; 128]
+        }
+    }
+}
+
+impl Output for TermionOutput {
+    fn set(&mut self, x: usize, y: usize, state: u8) {
+        let old_state = &mut self.cells[y][x];
+        if *old_state != state {
+            *old_state = state;
+            write!(self.screen, "{}", termion::cursor::Goto(2 * x as u16 + 1, y as u16 + 1)).unwrap();
+            write!(self.screen, "{}", if state == 1 { "##" } else { "  " }).unwrap();
+            self.screen.flush().unwrap();
+        }
+    }
+    fn get(&self, x: usize, y: usize) -> u8 {
+        self.cells[y][x]
+    }
+    fn clear(&mut self) {
+        self.cells = [[0; 128]; 128];
+        write!(self.screen, "{}", termion::clear::All).unwrap();
+        self.screen.flush().unwrap();
+    }
+    fn refresh(&mut self) {
+        write!(self.screen, "{}", termion::cursor::Goto(1, 1)).unwrap();
+        for row in self.cells.iter() {
+            for cell in row.iter() {
+                write!(self.screen, "{}", if *cell == 1 { "##" } else { "  " } ).unwrap();
+            }
+            writeln!(self.screen, "").unwrap();
+        }
+        self.screen.flush().unwrap();
+    }
+}
+
+fn event_listener(sender: mpsc::Sender<(u8, time::SystemTime)>, key_pressed: Arc<Mutex<Option<(u8, time::SystemTime)>>>, waiting: Arc<Mutex<bool>>, stop: Arc<Mutex<bool>>) -> thread::JoinHandle<()> {
+    thread::spawn(move || {
+        let stdin = std::io::stdin();
+        for event in stdin.keys() {
+            match event {
+                Ok(key) => {
+                    match key {
+                        Key::Char('q') | Key::Ctrl('c') => {
+                            *stop.lock().unwrap() = true; // Tell the main thread to end
+                            break; // Make this thread end
+                        }
+                        // Key::Ctrl('l') => {
+                        //     write!(stdout, "{}", termion::clear::All)?;
+                        //     emulator.flag_all_pixels_for_redrawing();
+                        // }
+                        Key::Char(c) => {
+                            if let Some(int_value) = c.to_digit(10) {
+                                if (0..0xF).contains(&int_value) {
+                                    // Make two individual critical sections
+                                    let key_with_timestamp = (int_value as u8, std::time::SystemTime::now());
+                                    *key_pressed.lock().unwrap() = Some(key_with_timestamp);
+                                    if *waiting.lock().unwrap() {
+                                        sender.send(key_with_timestamp).unwrap();
+                                    }
+                                }
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+                _ => {}
+            }
+        }
+    })
+}
+
 fn main() -> std::io::Result<()> {
 
     let logfile = FileAppender::builder()
@@ -48,68 +179,24 @@ fn main() -> std::io::Result<()> {
     log::info!("Executing {:?}", &opt.input);
     let program = std::fs::read(opt.input)?;
 
+    // Create shared data
+    let (sender, receiver) = mpsc::channel();
+    let key = Arc::new(Mutex::new(None));
+    let waiting = Arc::new(Mutex::new(false));
+    let stop = Arc::new(Mutex::new(false));
+    let event_listener = event_listener(sender, key.clone(), waiting.clone(), stop.clone());
+
     // Load instructions into emulator memory
-    let mut emulator = Emulator::new();
+    let mut emulator = Emulator::with_io(TermionInput::new(key.clone(), waiting.clone(), receiver), TermionOutput::new());
     emulator.load(&program);
 
-    // Create alternate screen to draw on
-    let mut stdout = AlternateScreen::from(stdout().into_raw_mode().unwrap());
-    let stdin = termion::async_stdin();
-    let mut events = stdin.events();
-
-    write!(stdout, "{}", termion::cursor::Hide)?;
-
-    let now = std::time::SystemTime::now();
-    let no_keys_pressed = [false; 0xF];
-    let mut keys_pressed = no_keys_pressed;
-    let mut timestamps = [now; 0xF];
-
     // Start execution
-    while emulator.step(&keys_pressed) {
-
-        // Buffer keypresses for a little while
-        for (is_pressed, timestamp) in keys_pressed.iter_mut().zip(timestamps.iter_mut()) {
-            if *is_pressed && timestamp.elapsed().expect("System time error") > std::time::Duration::from_millis(500) {
-                *is_pressed = false;
-                *timestamp = std::time::SystemTime::now();
-            }
-        }
-
-        // Check keys pressed
-        match events.next() {
-            Some(Ok(Event::Key(key))) => {
-                match key {
-                    Key::Char('q') => { break; }
-                    Key::Ctrl('c') => { break; }
-                    Key::Ctrl('l') => {
-                        write!(stdout, "{}", termion::clear::All)?;
-                        emulator.flag_all_pixels_for_redrawing();
-                    }
-                    Key::Char(c) => {
-                        if let Some(int_value) = c.to_digit(10) {
-                            if (0..0xF).contains(&int_value) {
-                                keys_pressed[int_value as usize] = true;
-                                timestamps[int_value as usize] = std::time::SystemTime::now();
-                            }
-                        }
-                    }
-                    _ => {}
-                }
-            },
-            _ => {}
-        }
-
-        // Write the virtual screen contents to the terminal
-        let screen = emulator.get_screen();
-        for &(x, y) in emulator.get_screen_update_locations() {
-            write!(stdout, "{}", termion::cursor::Goto(2 * x as u16 + 1, y as u16 + 1))?;
-            write!(stdout, "{}", if screen[y][x] == 1 { "##" } else { "  " })?;
-        }
-        stdout.flush()?;
-
-        // Sleep until the next frame
+    while !*stop.lock().unwrap() {
+        emulator.step();
         std::thread::sleep(std::time::Duration::from_millis(1_000/120));
     }
+
+    event_listener.join().expect("Could not join event listener");
 
     Ok(())
 }
