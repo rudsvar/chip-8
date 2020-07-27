89998dad4ec0542f4923a3a5efd2c28ffeb80bd8
Rudi B. Svartveit <rudsvar@gmail.com>
2020-07-27 22:12:28 +0200

    Add an empty frontend and test env_logger

---
+++ src/bin/no_frontend.rs
@@ -0,0 +1,34 @@
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+
+use chip_8::emulator::emulator::Emulator;
+use chip_8::emulator::emulator::{DummyInput, DummyOutput};
+
+/// The program options.
+#[derive(StructOpt)]
+struct Opt {
+    /// The program to execute
+    #[structopt(parse(from_os_str))]
+    input: PathBuf,
+}
+
+fn main() -> std::io::Result<()> {
+
+    env_logger::init();
+
+    // Get configuration and read input file
+    let opt = Opt::from_args();
+    log::info!("Executing {:?}", &opt.input);
+    let program = std::fs::read(opt.input)?;
+
+    // Load instructions into emulator memory
+    let mut emulator = Emulator::<DummyInput, DummyOutput>::new();
+    emulator.load(&program);
+
+    // Start execution
+    loop {
+        emulator.step();
+        std::thread::sleep(std::time::Duration::from_millis(1_000/60));
+    }
+}
